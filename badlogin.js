4a5742a0d8ce4a237b031f54ebee4c948f08e13a
Zack Grossbart <zack@grossbart.com>
2014-03-10 09:42:49 -0400

    Added a bunch of code comments and did some code cleanup.

--- badlogin.js
+++ badlogin.js
@@ -1,14 +1,29 @@
-z = {
-    drawHLine: function(x, y, length) {
+/*
+ * This object handles the bad logins drawings.  It's basically a utility object
+ * which knows how to draw the dots and handle the interactions with the graph.
+ */
+badLogins = {
+    /**
+     * This function handles drawing the lines which separate
+     * the swimlanes of the chart.
+     *
+     * @param x the x coordinate of the line
+     * @param y the y coordinate of the line
+     * @param length the length of the line
+     */
+    drawLine: function(x, y, length) {
         var path1 = "M " + x + " " + y + " L " + length + " " + y;
-        z.r.path(path1).attr({"stroke-width": 1, "stroke": "black"});
+        badLogins.r.path(path1).attr({"stroke-width": 1, "stroke": "black"});
     },
     
-    drawVLine: function(x, y, length) {
-        var path1 = "M " + x + " " + y + " L " + x + " " + length;
-        z.r.path(path1).attr({"stroke-width": 1, "stroke": "black"});
-    },
-    
+    /**
+     * Show the popup for a specific dot when the user hovers the 
+     * mouse over it.
+     *
+     * @param x the x coordinate to show the popup at
+     * @param y the y coordinate to show the popup at
+     * @param text the text of the popup
+     */
     showPopup: function(x, y, text) {
          x += 5;
          y += 5;
@@ -22,21 +37,41 @@ z = {
          }).show('fast');
     },
     
+    /**
+     * Hide the popup when the user moves their mouse somewhere else
+     */
     hidePopup: function() {
          $('#popup').hide('slow');
     },
     
+    /**
+     * This function adds a circle to the graph and handles the interactions 
+     * with that circle.
+     *
+     * @param x the x coordinate of the dot
+     * @param y the y coordinate of the dot
+     * @param radius the radius of the dot
+     * @param color the color of the dot
+     */
     circle: function(x, y, radius, color) {
          var inCircle = true;
-         return z.r.circle(x, y, radius).attr({fill: color, "stroke-width": 0})
+         return badLogins.r.circle(x, y, radius).attr({fill: color, "stroke-width": 0})
             .mouseover(function (event) {
                 inCircle = true;
                 this.animate({r: 8}, 100);
                 var boxX = this.getBBox().x - 50;
                 var boxY = this.getBBox().y;
                 
+                /*
+                 * We want to show the popup for the dot when the user hovers
+                 * their mouse over the dot and waits for a quarter of a second.
+                 */
                 setTimeout(function() {
                     if (inCircle) {
+                        /*
+                         * Here we have a little hacking to make the title show up properly
+                         * in the popup.
+                         */
                         var title = '';
                         if (y < 150) {
                             title = 'HR Database';
@@ -49,90 +84,89 @@ z = {
                         } else if (y < 550) {
                             title = 'PeopleSoft';
                         }
-                        z.output('boxX: ' + boxX);
                         var paperWidth = $('div.container').width();
-                        z.output('paperWidth: ' + paperWidth);
                         
                         if (boxX + 210 > paperWidth - 25) {
+                            /*
+                             * We want to pull the popup back a little bit if it's too
+                             * far to the right and ends up outside of the graph.
+                             */
                             boxX = paperWidth - 235;
                         }
                         
+                        /*
+                         * We want to show a different message for the gray dots which are 
+                         * successful logins and the colored dots which represents failed 
+                         * logins.
+                         */
                         if (color === 'darkgray') {
-                            z.showPopup(boxX, boxY, '<h4>' + title + '</h4>' + 
+                            badLogins.showPopup(boxX, boxY, '<h4>' + title + '</h4>' + 
                                 'admin logged into ' + title + ' from 24.61.131.44 on 11:15 AM, February 1');
                         } else {
-                            z.showPopup(boxX, boxY, '<h4>' + title + '</h4>' + 
+                            badLogins.showPopup(boxX, boxY, '<h4>' + title + '</h4>' + 
                                 'Someone from 24.61.131.44 tried to log in as admin on 11:15 AM, February 1');
                         }
                     }
                 }, 250);
             }).mouseout(function (event) {
+                /*
+                 * When the mouse goes out of the dot we want to close the popup
+                 */
                 if (inCircle) {
-                    z.hidePopup();
+                    badLogins.hidePopup();
                 }
                 inCircle = false;
                 this.animate({r: 3}, 200);
-            }).click(function (event) {
-                z.output("click...");
-            });
-         
-    },
-    
-    output: function(msg) {
-         if (window.console) {
-             console.info(msg);
-         }
-    }
-    
+            });    
+    }    
 }
-
 
 jQuery(document).ready(function(){
     
-    z.r = Raphael("holder", '100%', '100%');
+    badLogins.r = Raphael("holder", '100%', '100%');
     
     var width = $('#holder').width() - 50;
     
     for (var y = 50; y < 600; y += 100) {
-        z.drawHLine(100, y, width);
+        badLogins.drawLine(100, y, width);
     }
     
     /*
      * We'll start with all of our successfull logins
      */
     for (var i = 0; i < 20; i++) {
-        z.circle(Math.max(125, Math.floor(Math.random() * width)), 140, 3, "darkgray");
+        badLogins.circle(Math.max(125, Math.floor(Math.random() * width)), 140, 3, "darkgray");
     }
     
     for (var i = 0; i < 10; i++) {
-        z.circle(Math.max(125, Math.floor(Math.random() * width)), 240, 3, "darkgray");
+        badLogins.circle(Math.max(125, Math.floor(Math.random() * width)), 240, 3, "darkgray");
     }
     
     for (var i = 0; i < 23; i++) {
-        z.circle(Math.max(125, Math.floor(Math.random() * width)), 340, 3, "darkgray");
+        badLogins.circle(Math.max(125, Math.floor(Math.random() * width)), 340, 3, "darkgray");
     }
     
     for (var i = 0; i < 6; i++) {
-        z.circle(Math.max(125, Math.floor(Math.random() * width)), 440, 3, "darkgray");
+        badLogins.circle(Math.max(125, Math.floor(Math.random() * width)), 440, 3, "darkgray");
     }
     
     for (var i = 0; i < 14; i++) {
-        z.circle(Math.max(125, Math.floor(Math.random() * width)), 540, 3, "darkgray");
+        badLogins.circle(Math.max(125, Math.floor(Math.random() * width)), 540, 3, "darkgray");
     }
     
     /*
      * Now a user with a bad password in Windows
      */
     for (var i = 0; i < 200; i++) {
-        z.circle(Math.max(125, Math.floor(Math.random() * width)), 60, 3, "#fec01e");
+        badLogins.circle(Math.max(125, Math.floor(Math.random() * width)), 60, 3, "#fec01e");
     }
     
     for (var i = 0; i < 200; i++) {
-        z.circle(Math.max(125, Math.floor(Math.random() * width)), 160, 3, "#fec01e");
+        badLogins.circle(Math.max(125, Math.floor(Math.random() * width)), 160, 3, "#fec01e");
     }
     
     for (var i = 0; i < 200; i++) {
-        z.circle(Math.max(125, Math.floor(Math.random() * width)), 460, 3, "#fec01e");
+        badLogins.circle(Math.max(125, Math.floor(Math.random() * width)), 460, 3, "#fec01e");
     }
     
     /*
@@ -145,9 +179,9 @@ jQuery(document).ready(function(){
         y -= 5;
         
         if (i == 4) {
-            z.circle(x, y, 3, "darkgray");
+            badLogins.circle(x, y, 3, "darkgray");
         } else {
-            z.circle(x, y, 3, "#f69c9f");         // pink
+            badLogins.circle(x, y, 3, "#f69c9f");         // pink
         }
     }
     
@@ -157,11 +191,11 @@ jQuery(document).ready(function(){
     x = Math.floor(width / 3);
     for (var i = 0; i < 3; i++) {
         x += Math.floor(Math.random() * 15);
-        z.circle(x, 140, 3, "#00aeef");         // yellow
+        badLogins.circle(x, 140, 3, "#00aeef");         // yellow
     }
     
     x += Math.floor(width / 10);
-    z.circle(x, 130, 3, "#00aeef");             // yellow
+    badLogins.circle(x, 130, 3, "#00aeef");             // yellow
     
     
     /*
@@ -174,9 +208,9 @@ jQuery(document).ready(function(){
         y -= 5;
         
         if (i == 9) {
-            z.circle(x, y, 3, "darkgray");
+            badLogins.circle(x, y, 3, "darkgray");
         } else {
-            z.circle(x, y, 3, "#52b755");           // green
+            badLogins.circle(x, y, 3, "#52b755");           // green
         }
     }
     
@@ -189,11 +223,11 @@ jQuery(document).ready(function(){
         x += Math.max(10, Math.floor(Math.random() * 20));
         y -= 5;
         
-        z.circle(x, y, 3, "#e0cb61");           // beige
+        badLogins.circle(x, y, 3, "#e0cb61");           // beige
     }
     
     x += Math.max(60, Math.floor(Math.random() * 80));
-    z.circle(x, y - 5, 3, "darkgray");           // beige
+    badLogins.circle(x, y - 5, 3, "darkgray");           // beige
     
     /*
      * Now the blue dots
@@ -205,9 +239,9 @@ jQuery(document).ready(function(){
         y -= 5;
         
         if (i == 4) {
-            z.circle(x, y, 3, "darkgray");
+            badLogins.circle(x, y, 3, "darkgray");
         } else {
-            z.circle(x, y, 3, "#157d6b");           // blue
+            badLogins.circle(x, y, 3, "#157d6b");           // blue
         }
     }
     
@@ -220,7 +254,7 @@ jQuery(document).ready(function(){
         x += Math.floor(Math.random() * 30);
         y -= 3;
         
-        z.circle(x, y, 3, "#ee2a33");           // red
+        badLogins.circle(x, y, 3, "#ee2a33");           // red
     }
     
     /*
@@ -233,9 +267,9 @@ jQuery(document).ready(function(){
         y -= 3;
         
         if (i == 21) {
-            z.circle(x, y, 3, "darkgray");
+            badLogins.circle(x, y, 3, "darkgray");
         } else {
-            z.circle(x, y, 3, "#ee2a33");          // red
+            badLogins.circle(x, y, 3, "#ee2a33");          // red
         }
     }
     
@@ -249,9 +283,9 @@ jQuery(document).ready(function(){
         y -= 3;
         
         if (i == 27) {
-            z.circle(x, y, 3, "darkgray");
+            badLogins.circle(x, y, 3, "darkgray");
         } else {
-            z.circle(x, y, 3, "#ee2a33");          // red
+            badLogins.circle(x, y, 3, "#ee2a33");          // red
         }
     }
     
@@ -262,7 +296,7 @@ jQuery(document).ready(function(){
     var y = 340;
     for (var i = 0; i < 21; i++) {
         x += Math.max(30, Math.floor(Math.random() * 50));
-        z.circle(x, y, 3, "#ee2a33");           // red    
+        badLogins.circle(x, y, 3, "#ee2a33");           // red    
     }
     
     /*
@@ -273,7 +307,7 @@ jQuery(document).ready(function(){
     for (var i = 0; i < 3; i++) {
         x += Math.max(20, Math.floor(Math.random() * 30));
         y -= 5;
-        z.circle(x, y, 3, "#f69c9f");           // pink
+        badLogins.circle(x, y, 3, "#f69c9f");           // pink
     }
     
     /*
@@ -284,6 +318,6 @@ jQuery(document).ready(function(){
     for (var i = 0; i < 3; i++) {
         x += Math.max(40, Math.floor(Math.random() * 80));
         y -= 5;
-        z.circle(x, y, 3, "#F00FF0");           // purple
+        badLogins.circle(x, y, 3, "#F00FF0");           // purple
     }
 });
